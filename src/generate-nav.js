1c3a5d8be66a15fb55fc11a2c96812b36513ed6f
Ufuk Bakan <println.ufukbakan@gmail.com>
2024-10-06 16:37:52 +0300

    feat: categorize sub pages in navigation

--- src/generate-nav.js
+++ src/generate-nav.js
@@ -1,9 +1,30 @@
 import { onMenuPages } from "../index.js";
 
-export default function generateNavigation(lang){
-    return onMenuPages.map(({url, metas}) => {
-        const meta = metas.find(meta => meta.lang === lang) || metas[0];
-        const title = meta.title || new URL(url).pathname.replace('/', '');
-        return `<li><a href="${url}">${title}</a></li>`
-    }).join('');
+export default function generateNavigation(){
+  function buildMenu(pages, parentUrl = '') {
+    let menu = '';
+
+    pages
+      .filter(page => page.url.startsWith(parentUrl)) // Get child pages
+      .forEach(page => {
+        const title = page.url.split('/').pop().replace(/-/g, ' ').replace(/\b\w/g, c => c.toUpperCase()); // Create title from URL
+        let li = `<li class="pure-menu-item"><a class="pure-menu-link" href="${page.url}">${title}</a>`;
+
+        // Find if there are subpages and recursively build the submenu
+        const subPages = onMenuPages.filter(subPage => subPage.url.startsWith(`${page.url}/`));
+        if (subPages.length > 0) {
+          li += '<ul>'
+          li += buildMenu(subPages, page.url); // Add sub-menu inside the <li> if there are subpages
+          li += "</ul>"
+        }
+
+        li += '</li>';
+        menu += li;
+      });
+    return menu;
+  }
+
+  // Get only top-level pages (without slashes beyond the root)
+  const topLevelPages = onMenuPages.filter(page => page.url.split('/').length === 2);
+  return buildMenu(topLevelPages);
 }

49dea880b1565c6562682c0471cf439aa8b7643d
Ufuk Bakan <println.ufukbakan@gmail.com>
2024-10-07 13:14:11 +0300

    feat: sub heading on table of contents

--- default-env.js
+++ default-env.js
@@ -1,5 +1,6 @@
 export default {
     PORT: 3000,
     ENCODING: 'utf-8',
-    DEFAULT_LANG: 'en'
+    DEFAULT_LANG: 'en',
+    DEFAULT_CONTENT_HEADING_LEVEL: 2,
 }
--- src/genereate-toc.js
+++ src/genereate-toc.js
@@ -1,14 +1,49 @@
 import { load } from "cheerio";
 import dictionary from "../dictionary.js";
+import config from "../config.js";
 
-export default function generateTableOfContents(html, lang){
+export default function generateTableOfContents(html, lang, level = config.DEFAULT_CONTENT_HEADING_LEVEL, parent = null, parentLevel = null) {
     const $ = load(html);
-    let toc = `<h2>${dictionary.toc[lang]}</h2><ul/>`;
-    $('h1[id]').each(function(){
+    if(parent){
+        return generateSubTableOfContents($, lang, level, parent, parentLevel);
+    }
+    let toc = '';
+    $(`h${level}[id]`).each(function () {
         const text = $(this).text();
         const id = $(this).attr('id');
-        toc += `<li><a href="#${id}">${text}</a></li>`;
+        toc += `<li><a href="#${id}">${text}</a>`;
+        toc += generateTableOfContents(html, lang, level+1, $(this), level);
+        toc += '</li>';
     });
-    toc += '</ul>';
+    if(toc.length > 0){
+        toc = `<h2>${dictionary.toc[lang]}</h2><ol>${toc}</ol>`
+    }
     return toc;
+}
+
+/**
+ * 
+ * @param {import("cheerio").CheerioAPI} $ 
+ * @param {string} lang 
+ * @param {number} level 
+ * @param {import("cheerio").Cheerio<Element>} parent 
+ * @param {number} parentLevel 
+ */
+function generateSubTableOfContents($, lang, level, parent, parentLevel){
+    let nextElement = parent.next();
+    let toc = "<ol>";
+    while(nextElement.length){
+        if(nextElement.is(`h${parentLevel}`)){
+            break;
+        }
+        if(nextElement.is(`h${level}[id]`)){
+            const text = nextElement.text();
+            const id = nextElement.attr('id');
+            toc += `<li><a href="#${id}">${text}</a>`;
+            toc += generateSubTableOfContents($, lang, level+1, nextElement, parentLevel+1);
+            toc += "</li>"
+        }
+        nextElement = nextElement.next();
+    }
+    return toc + "</ol>";
 }
